41855cc1827efdfd9964e37238418179469e5bb9
sorattorafa <38047989+sorattorafa@users.noreply.github.com>
2023-05-19 13:36:26 -0300

    Update failure_parser.py

--- shaker/failure_parser.py
+++ shaker/failure_parser.py
@@ -9,27 +9,27 @@ def order(entry):
         return (int(entry["config"]), int(entry["run_number"]))
 
 def find_xml_files(dir):
-    list = []
+    xml_files = []
     if(len(list(Path(dir.absolute().name).rglob('*.xml'))) != 0):
         print(1, list(Path(dir.absolute().name).rglob('*.xml')))
-        list = list(Path(dir.absolute().name).rglob('*.xml'))
+        xml_files = list(Path(dir.absolute().name).rglob('*.xml'))
     elif(len(list(Path(dir).rglob('*.xml'))) != 0):
         print(2, list(Path(dir).rglob('*.xml')))
-        list = list(Path(dir).rglob('*.xml'))
+        xml_files = list(Path(dir).rglob('*.xml'))
     elif(len(list(Path(dir.name).rglob('*.*.xml'))) != 0):
         print(3)
-        list = list(Path(dir.name).rglob('*.*.xml'))
+        xml_files = list(Path(dir.name).rglob('*.*.xml'))
     elif(len(list(Path(dir.name).rglob('*.*.xml'))) != 0):
         print(4)
-        list = list(Path(dir.name).rglob('*.xml'))
+        xml_files = list(Path(dir.name).rglob('*.xml'))
     elif(len(list(Path(dir).rglob('*.*.xml'))) != 0):
         print(5)
-        list = list(Path(dir).rglob('*.*.xml'))
+        xml_files = list(Path(dir).rglob('*.*.xml'))
     elif(len(list(Path(dir).rglob('*.*.xml'))) != 0):
         print(6)
-        list = list(Path(dir).rglob('*.xml'))
-    print(list, 'list.xml')
-    return list
+        xml_files = list(Path(dir).rglob('*.xml'))
+    print(xml_files, 'list.xml')
+    return xml_files
     
 # Parses all xml files in the project folder
 def parse(dir):
